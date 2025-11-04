b189a131d638e9f31a1089c22c69ffedfca577f7
SVatsa12 <qshubhamq2@gmail.com>
2025-11-05 00:09:20 +0530

    new commit

---
+++ .dist/AssignmentOperator.java
@@ -0,0 +1,22 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int A=10;
+        System.out.println("value of a:"+A);
+        A+=10;
+        System.out.println("+= gives:"+A);
+        int b=20;
+        System.out.println("value of b:"+b);
+        b-=5;
+        System.out.println("-= gives:"+b);
+        int c=30;
+        System.out.println("value of c:"+c);
+        c*=5;
+        System.out.println("*= gives:"+c);
+        int d=50;
+        System.out.println("value of d:"+d);
+        d/=5;
+        System.out.println("/= gives:"+d);
+
+    }
+}
+
