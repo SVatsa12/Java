695105b5f2666a460e6694128566980bc1fd13b7
SVatsa12 <qshubhamq2@gmail.com>
2025-11-10 01:42:31 +0530

    Forloop

---
+++ .dist/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("ForLoop syntax:");
+        for(int i=1;i<=9;i++){
+            System.out.println(i);
+        }
+    }
+}
