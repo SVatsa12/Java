32ca671b0788d32948264e8d46592db2aef49200
SVatsa12 <qshubhamq2@gmail.com>
2025-11-28 21:12:05 +0530

    conditional statements

---
+++ .dist/Conditional.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Conditional {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age=sc.nextInt();
+        if(age>18){
+            System.out.println("adult");
+        }else if(age>13 && age<17){
+            System.out.println("Adolescent");
+        }
+        else{
+            System.out.println("kids");
+        }
+        sc.close();
+    }
+    
+}
---
+++ .dist/comments.java
@@ -0,0 +1,10 @@
+public class comments {
+    public static void main(String[] args) {
+        //Single line comment starts with://
+        // Multiline comments:multiline comments starts with '/*' and ends with '*/'
+        //single line comment example:code to calculate sum
+        /*Multiline comment example: we are going to code for
+        finding sum of two numbers
+         */
+    }
+}
