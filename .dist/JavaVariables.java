b8b4b9d05a3506014745353ba777d8fbf1ea1f71
SVatsa12 <qshubhamq2@gmail.com>
2025-11-28 21:22:37 +0530

    variables

---
+++ .dist/JavaVariables.java
@@ -0,0 +1,50 @@
+
+
+public class JavaVariables {
+    public static void main(String args []){
+        int a=10;
+        int b=5;
+        System.out.println("a");
+        System.out.println(a);
+        System.out.println(b);
+        String name="Alex";
+        System.out.println(name);
+        b=72;
+        System.out.println(b);
+        name="May";
+        System.out.println(name);
+
+        //Datatypes in Java
+        /*
+        Primitive datatypes:
+        datatypes - size
+        byte- 1byte
+        short- 2byte
+        char-  2byte
+        boolean- 1byte,
+        int-   4bytes
+        long-   8bytes
+        float-  4bytes
+        double- 8bytes
+        */
+        //Non-Primitive datatypes:string,array,class,object,interface
+        byte x=8;
+        System.out.println(x);
+        boolean var=true;
+        System.out.println(var);
+        float y=10.5f;
+        double z=97.7;
+        //long
+        //short
+        char ch='z';
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(ch);
+
+
+
+
+
+
+    }
+}
